cfb5b739e34c385226a53aa7fd182bc375bdb33a
ksteussy <ksteuss@gmail.com>
2021-05-04 19:57:37 -0500

    Added load_credentials() function

--- dropbox_oauth_json.py
+++ dropbox_oauth_json.py
@@ -2,17 +2,21 @@ import dropbox
 import webbrowser
 import json
 
-try:
-    with open('creds.json', 'r') as f:
-        oauth_creds = json.load(f)
-except FileNotFoundError as e:
-    print('ERROR: Unable to load credentials\n' + str(e))
-    exit()
-else:
-    print('Opened creds.json...')
+KEY_FILE = 'creds.json'
 
-app_key = oauth_creds['app_key']
-app_secret = oauth_creds['app_secret']
+
+def load_credentials():
+    # Function to load the app_key and app_secret from key file
+    try:
+        with open(KEY_FILE, 'r') as f:
+            oauth_creds = json.load(f)
+    except FileNotFoundError as e:
+        print('ERROR: Unable to load credentials\n' + str(e))
+        exit()
+    else:
+        print('Loading creds.json...')
+        print('creds.json loaded...')
+        return oauth_creds
 
 
 def dbx_oauth_flow(key, secret):
@@ -33,7 +37,13 @@ def dbx_oauth_flow(key, secret):
         print('Error: ' + e)
 
 
-# Store the results of the oauth flow
+# Load credentials from the json file and store.
+oauth_creds = load_credentials()
+app_key = oauth_creds['app_key']
+app_secret = oauth_creds['app_secret']
+
+# Start the oauth flow and store the results
+print('Starting the OAuth flow...')
 oauth_result = dbx_oauth_flow(app_key, app_secret)
 
 access_token = oauth_result.access_token
@@ -43,5 +53,5 @@ oauth_creds['refresh_token'] = refresh_token
 oauth_creds['access_token'] = access_token
 
 # Save the refresh token and access token to json file
-with open('creds.json', 'w') as f:
+with open(KEY_FILE, 'w') as f:
     json.dump(oauth_creds, f)
